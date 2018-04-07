f30621f6d80cb03856eb27be391d452f30c35a3a
FPowrie <36450890+FPowrie@users.noreply.github.com>
2018-04-07 21:27:02 +0100

    Various changes
    -Border/background of form
    -Submission and response
    -Radio and sub-question

--- dynamic.js
+++ dynamic.js
@@ -1,3 +1,25 @@
 $(document).ready(function() {
   $("#datepicker").datepicker();
 });
+
+
+
+$(document).ready(function(){
+    $("#button1").click(function(){
+        $("#replacement").toggle();
+        $("#form1").toggle();
+        $(".explanation").toggle();
+    });
+});
+
+$(document).ready(function(){
+    $("#offerride").click(function(){
+        $("#liftq").show();
+    });
+});
+
+$(document).ready(function(){
+    $("#needride").click(function(){
+        $("#liftq").hide();
+    });
+});
